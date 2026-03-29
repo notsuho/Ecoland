319490f3e7a7f8a838d396c0cacfb3e03dae0638
AleksiKantola3 <aleksi3472@gmail.com>
2026-03-29 14:45:56 +0300

    Changes from main push

---
+++ Assets/Scenes/Minigames/Econews/NewsItem.cs
@@ -0,0 +1,9 @@
+/*Structure for news article items*/
+public class NewsItem
+{
+    public string title;
+    public string description;
+    public string link;
+    public string category;
+    public string pubDate;
+}
