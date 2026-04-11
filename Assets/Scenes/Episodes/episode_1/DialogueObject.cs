bc9bb835a8ce0f95955e934157b550bc2e072b59
Sampsa Jalli <sampsa.jalli@gmail.com>
2026-04-11 20:02:37 +0300

    Finalizing work on episode 1
    Cleaned up a bit by creating a separate controller for the UI, moved things out of Scene Controller to make it less cluttered. Added new scenes and dialogue objects for the rest of the episode flow, also updated the structure for the dialogue object a bit. Added reward chest button for the ending to display.

--- Assets/Scenes/Episodes/episode_1/DialogueObject.cs
+++ Assets/Scenes/Episodes/episode_1/DialogueObject.cs
@@ -1,12 +1,31 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 [CreateAssetMenu(fileName = "DialogueObject", menuName = "Scriptable Objects/DialogueObject")]
 public class DialogueObject : ScriptableObject
 {
-    //For storing NPC dialogue to load for use in the dialoguebox text fields
-    //Will see about supporting branching in future, keeping it simple for now
+    // For storing NPC dialogue to load for use in the dialoguebox text fields
+    // Will see about supporting branching in future, keeping it simple for now
 
     [TextArea(10, 100)]
     public string[] dialogue;
+    public bool needtoggle;
+    public Sprite left;
+    public Sprite right;
+
+    public Choice[] choices;
+
+    // Used if no choices (linear dialogue)
+    public DialogueObject nextNode;
+    public bool isChoice; // boolean flag for if this is a choice node
+    public bool isLast; // boolean flag for if this is the last one in episode
+    // Optional: scene transition
+    public string sceneToLoad; // name of next scene to load after this dialogue node if needed
+
+    [System.Serializable]
+    public class Choice
+    {
+        public DialogueObject nextNode;
+    }
 
 }
--- Assets/Scenes/Episodes/episode_1/SceneController.cs
+++ Assets/Scenes/Episodes/episode_1/SceneController.cs
@@ -1,61 +1,38 @@
 using TMPro;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
 
-    public GameObject rightPortrait;
-    public GameObject leftPortrait;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void LoadScene(string name)
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void ToggleVisibility()
-    {
-        //WIP for toggling visibility of NPC avatars
-        if (rightPortrait.activeSelf && leftPortrait.activeSelf)
-        {
-            rightPortrait.SetActive(false);
-            leftPortrait.SetActive(false);
-        }
-        else
-        {
-            rightPortrait.SetActive(true);
-            leftPortrait.SetActive(true);
-        }
-        
+        // Loads a branch by name, passed as string
+        SceneManager.LoadScene(name);
     }
 
     public void LoadBranch(GameObject button)
     {
-        //Called by the on click method attached to the branch choice buttons.
-        //Receives as argument which button was pressed, so we can then
-        //decide which next story branch scene to load based on the choice.
+        // Called by the on click method attached to the branch choice buttons.
+        // Receives as argument which button was pressed, so we can then
+        // decide which next story branch scene to load based on the choice.
+        
         Debug.Log(button.name.ToString() + " was pressed.");
 
         switch (button.name.ToString())
         {
             case "Left Choice":
-            SceneManager.LoadScene("episode_1_choice_1");
-            break;
+                SceneManager.LoadScene("episode_1_choice_1");
+                break;
             case "Right Choice":
-            SceneManager.LoadScene("episode_1_choice_2");
-            break;
+                SceneManager.LoadScene("episode_1_choice_2");
+                break;
             default:
-            Debug.Log("Default choice, nothing will happen.");
-            break;
+                Debug.Log("Default choice, nothing will happen.");
+                break;
         }
 
     }
